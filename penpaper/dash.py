e0fd87d9cd295945ae27628432edc4a0ff00c8da
Zoe Liu <zliu18@berkeley.edu>
2019-10-26 17:12:26 -0700

    fixed the big issue???

--- penpaper/dash.py
+++ penpaper/dash.py
@@ -9,3 +9,7 @@ bp = Blueprint('dash', __name__, url_prefix='/dash')
 @bp.route('', methods=('GET','POST'))
 def index():
 	return render_template('dash/index.html')
+
+@bp.route('/calendar')
+def calendar():
+	return render_template('auth/calendar.html')
